aeab59d03c1bafbaa92b76de84cad35374d75a9d
moacirjun <moacirjunior.eletro@gmail.com>
2022-02-24 10:46:17 -0300

    Tratar resposta de erro do PicPay, Arrumar logo do PicPay no checkout.

--- Gateway/Response/TxnIdHandler.php
+++ Gateway/Response/TxnIdHandler.php
@@ -2,25 +2,46 @@
 
 namespace Picpay\Payment\Gateway\Response;
 
+use InvalidArgumentException;
+use Magento\Framework\Exception\LocalizedException;
 use Magento\Payment\Gateway\Data\PaymentDataObjectInterface;
 use Magento\Payment\Gateway\Response\HandlerInterface;
+use Psr\Log\LoggerInterface;
 
 class TxnIdHandler implements HandlerInterface
 {
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    /**
+     * @param LoggerInterface $logger
+     */
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
     /**
      * Handles transaction id
      *
      * @param array $handlingSubject
      * @param array $response
      * @return void
+     *
+     * @throws LocalizedException
+     * @throws InvalidArgumentException
      */
     public function handle(array $handlingSubject, array $response)
     {
         if (!isset($handlingSubject['payment'])
             || !$handlingSubject['payment'] instanceof PaymentDataObjectInterface
         ) {
-            throw new \InvalidArgumentException('Payment data object should be provided');
+            throw new InvalidArgumentException('Payment data object should be provided');
         }
+
+        $this->validatePicPayResponse($response);
 
         /** @var PaymentDataObjectInterface $paymentDO */
         $paymentDO = $handlingSubject['payment'];
@@ -30,9 +51,65 @@ class TxnIdHandler implements HandlerInterface
         $payment->getOrder()->setCanSendNewEmailFlag(true);
 
         /** @var $payment \Magento\Sales\Model\Order\Payment */
-        $payment->setTransactionId($response["return"]["referenceId"]);
+        $payment->setTransactionId($response['return']['referenceId']);
         $payment->setIsTransactionClosed(false);
-        $payment->setAdditionalInformation("paymentUrl", $response["return"]["paymentUrl"]);
-        $payment->setAdditionalInformation("qrcode", $response["return"]["qrcode"]);
+        $payment->setAdditionalInformation('paymentUrl', $response['return']['paymentUrl']);
+        $payment->setAdditionalInformation('qrcode', $response['return']['qrcode']);
+    }
+
+    /**
+     * @param array $response
+     * @return void
+     * @throws InvalidArgumentException
+     */
+    private function validatePicPayResponse(array $response): void
+    {
+        try {
+            $this->validateRequiredFields($response);
+        } catch (InvalidArgumentException $exception) {
+            $context = [
+                'message' => $exception->getMessage(),
+            ];
+
+            if (isset($response['return']['errors'])) {
+                $context['errors'] = array_column($response['return']['errors'], 'message');
+            }
+
+            $this->logger->error('picpay payment error', $context);
+
+            throw $exception;
+        }
+    }
+
+    private function validateRequiredFields(array $response): void
+    {
+        $errorMessage = $response['return']['message'] ?? null;
+
+        if ($response['success'] === 0) {
+            throw new InvalidArgumentException("Unexpected payment error. Details [$errorMessage]");
+        }
+
+        if (!$this->isValidStringField($response, 'referenceId')) {
+            throw new InvalidArgumentException($errorMessage ?? 'Resposta sem o campo referenceId');
+        }
+
+        if (!$this->isValidStringField($response, 'paymentUrl')) {
+            throw new InvalidArgumentException($errorMessage ?? 'Resposta sem o campo paymentUrl');
+        }
+
+        if (!isset($response['return']['qrcode']['content'])) {
+            throw new InvalidArgumentException($errorMessage ?? 'Resposta sem o campo qrcode.content');
+        }
+
+        if (!isset($response['return']['qrcode']['base64'])) {
+            throw new InvalidArgumentException($errorMessage ?? 'Resposta sem o campo qrcode.base64');
+        }
+    }
+
+    private function isValidStringField(array $response, string $field): bool
+    {
+        return isset($response['return'][$field])
+            && is_string($response['return'][$field])
+            && strlen(trim($response['return'][$field])) > 0;
     }
 }
--- Model/PicpayInstructionsConfigProvider.php
+++ Model/PicpayInstructionsConfigProvider.php
@@ -4,6 +4,7 @@ namespace Picpay\Payment\Model;
 
 use Magento\Checkout\Model\ConfigProviderInterface;
 use Magento\Framework\Escaper;
+use Magento\Framework\View\Asset\Repository as AssetsRepository;
 use Picpay\Payment\Helper\Data as PaymentHelper;
 
 class PicpayInstructionsConfigProvider implements ConfigProviderInterface
@@ -18,16 +19,24 @@ class PicpayInstructionsConfigProvider implements ConfigProviderInterface
      * @var PaymentHelper
      */
     protected $paymentHelper;
+
+    /**
+     * @var AssetsRepository
+     */
+    protected $assetsRepository;
+
     /**
      * @param PaymentHelper $paymentHelper
      * @param Escaper $escaper
      */
     public function __construct(
         PaymentHelper $paymentHelper,
-        Escaper $escaper
+        Escaper $escaper,
+        AssetsRepository $assetsRepository
     ) {
         $this->escaper = $escaper;
         $this->paymentHelper = $paymentHelper;
+        $this->assetsRepository = $assetsRepository;
     }
 
     /**
@@ -49,18 +58,17 @@ class PicpayInstructionsConfigProvider implements ConfigProviderInterface
      */
     protected function getInstructions()
     {
-        $instructions = "";
         if($this->paymentHelper->useCustomForm()) {
-            $instructions = $this->paymentHelper->getCustomHtmlForm();
+            return $this->paymentHelper->getCustomHtmlForm();
         }
-        else {
-            $instructions = '<img width="150px" src="https://ecommerce.picpay.com/doc/assets/picpay-logo.svg" alt="PicPay" '
-                . 'style="background-color: rgb(33, 194, 94); border: 0; padding: 10px;" />'
-                . '<br/>'
-                . '<p>Não conhece o PicPay? '
-                . '<a href="https://www.picpay.com/site" target="_blank">Clique aqui</a>'
-                . ' e baixe agora para efetuar seu pagamento.</p>';
-        }
-        return $instructions;
+
+        $logoAddress = $this->assetsRepository->getUrl('Picpay_Payment::images/picpay-logo.svg');
+
+        return '<img width="150px" src="' . $logoAddress . '" alt="PicPay Logo" '
+            . 'style="background-color: rgb(33, 194, 94); border: 0; padding: 10px;" />'
+            . '<br/>'
+            . '<p>Não conhece o PicPay? '
+            . '<a href="https://www.picpay.com/site" target="_blank">Clique aqui</a>'
+            . ' e baixe agora para efetuar seu pagamento.</p>';
     }
 }

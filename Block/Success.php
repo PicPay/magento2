a0b9344a9fb0a37a18a126dcd8baa030572aef7c
Thiago Contardi <thiagocontardi@hotmail.com>
2023-12-09 06:17:03 -0300

    i18n: remove duplicated translation

--- Block/Success.php
+++ Block/Success.php
@@ -3,11 +3,9 @@
 namespace Picpay\Payment\Block;
 
 use Magento\Checkout\Model\Session;
-use Magento\Customer\Model\Context;
 use Magento\Framework\View\Element\Template;
 use Magento\Sales\Model\Order;
 use Picpay\Payment\Helper\Data;
-
 
 class Success extends \Magento\Framework\View\Element\Template
 {
@@ -26,37 +24,23 @@ class Success extends \Magento\Framework\View\Element\Template
      */
     protected $order;
 
-    /**
-     * Success constructor.
-     * @param Template\Context $context
-     * @param Data $helper
-     * @param Session $checkoutSession
-     * @param array $data
-     */
     public function __construct(
         Template\Context $context,
         Data $helper,
         Session $checkoutSession,
         array $data = []
-    )
-    {
+    ) {
         $this->_checkoutSession = $checkoutSession;
         $this->helper = $helper;
         $this->order = $this->_checkoutSession->getLastRealOrder();
         parent::__construct($context, $data);
     }
 
-    /**
-     * @return Data
-     */
-    public function getHelper()
+    public function getHelper(): Data
     {
         return $this->helper;
     }
 
-    /**
-     * @return Order
-     */
     public function getOrder()
     {
         return $this->order;
